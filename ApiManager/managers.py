afe0d504852a1dabdbb6923080f21f74aa9eea37
尹全旺 <yinquanwang@weibopay.com>
2018-01-15 13:22:23 +0800

    修复用例信息存储数据库中文字符为16进制bug

--- ApiManager/managers.py
+++ ApiManager/managers.py
@@ -1,5 +1,3 @@
-import json
-
 from django.db import models
 
 '''用户类型表操作'''
@@ -50,12 +48,12 @@ class ProjectInfoManager(models.Manager):
 
         obj.save()
 
-    def get_pro_name(self, pro_name, type=True, id = None):
+    def get_pro_name(self, pro_name, type=True, id=None):
         if type:
             return self.filter(pro_name__exact=pro_name).count()
         else:
             if id is not None:
-                return self.get(id = int(id)).pro_name
+                return self.get(id=int(id)).pro_name
             return self.get(pro_name__exact=pro_name)
 
     def get_pro_info(self, type=True):
@@ -75,7 +73,7 @@ class ModuleInfoManager(models.Manager):
                     simple_desc=simple_desc, other_desc=other_desc)
 
     def update_module(self, id, module_name, test_user, lifting_time, simple_desc, other_desc):
-        obj = self.get(id = int(id))
+        obj = self.get(id=int(id))
         obj.module_name = module_name
         obj.test_user = test_user
         obj.lifting_time = lifting_time
@@ -84,12 +82,12 @@ class ModuleInfoManager(models.Manager):
 
         obj.save()
 
-    def get_module_name(self, module_name, type=True, id = None, project = None):
+    def get_module_name(self, module_name, type=True, id=None, project=None):
         if type:
             return self.filter(module_name__exact=module_name).count()
         else:
-            if id is not  None:
-                return self.get(id = int(id)).module_name
+            if id is not None:
+                return self.get(id=int(id)).module_name
             return self.get(belong_project__pro_name__exact=project, module_name__exact=module_name)
 
     def get_module_info(self, belong_project):
@@ -106,13 +104,17 @@ class TestCaseInfoManager(models.Manager):
         case_info = kwargs.get('test').pop('case_info')
         self.create(name=kwargs.get('test').get('name'), belong_project=case_info.pop('project'),
                     belong_module=belong_module,
-                    author=case_info.pop('author'), include=case_info.pop('include'), request=json.dumps(kwargs))
+                    author=case_info.pop('author'), include=case_info.pop('include'), request=kwargs)
 
     def insert_config(self, belong_module, **kwargs):
         config_info = kwargs.get('config').pop('config_info')
         self.create(name=kwargs.get('config').get('name'), belong_project=config_info.pop('project'),
                     belong_module=belong_module,
-                    author=config_info.pop('config_author'), type=2, request=json.dumps(kwargs))
+                    author=config_info.pop('config_author'), type=2, request=kwargs)
 
     def get_case_name(self, name, module_name, belong_project):
-        return self.filter(belong_module__module_name=module_name).filter(name__exact=name).filter(belong_project__exact=belong_project).count()
+        return self.filter(belong_module__module_name=module_name).filter(name__exact=name).filter(
+            belong_project__exact=belong_project).count()
+
+    def get_case_by_id(self, index):
+        return self.filter(id=index).all()
