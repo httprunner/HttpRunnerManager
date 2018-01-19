77f041a0f39cd8a90b2d7c7ac93814c38e51ffd1
尹全旺 <yinquanwang@weibopay.com>
2018-01-19 12:38:47 +0800

    urls 分发优化

--- HttpRunnerManager/activator.py
+++ HttpRunnerManager/activator.py
@@ -2,18 +2,22 @@ from django.shortcuts import HttpResponse
 
 
 def process(request, **kwargs):
-    app = kwargs.get('app', None)
-    fun = kwargs.get('function', None)
-
+    app = kwargs.pop('app', None)
+    fun = kwargs.pop('function', None)
+    index = kwargs.pop('id', None)
+    if app == 'api':
+        app = 'ApiManager'
     try:
         app = __import__("%s.views" % app)
         view = getattr(app, 'views')
         fun = getattr(view, fun)
 
         # 执行view.py中的函数，并获取其返回值
-        result = fun(request, kwargs)
-
+        result = fun(request, index)
+    except TypeError:
+        result = fun(request)
     except (ImportError, AttributeError):
         # 导入失败时，自定义404错误
         return HttpResponse('404 Not Found')
+
     return result
--- HttpRunnerManager/urls.py
+++ HttpRunnerManager/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 
+from HttpRunnerManager.activator import process
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('ApiManager.urls')),
-
+    url('^(?P<app>(\w+))/(?P<function>(\w+))/$', process),
+    url('^(?P<app>(\w+))/(?P<function>(\w+))/(?P<id>(\w+))/$', process),
 
 ]
