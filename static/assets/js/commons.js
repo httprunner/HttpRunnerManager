e0d33ebb6010bcc7eaca6537a50163aa90c2519d
尹全旺 <yinquanwang@weibopay.com>
2018-02-07 16:31:19 +0800

    修复增加项目bug

--- static/assets/js/commons.js
+++ static/assets/js/commons.js
@@ -13,8 +13,10 @@ function show_module(module_info) {
 /*表单信息异步传输*/
 function info_ajax(id) {
     var data = $(id).serializeJSON();
-    var url = '/api/add_project/';
-    if (id === '#add_module') {
+    var url;
+    if(id === '#add_project') {
+        url = '/api/add_project/';
+    }else if (id === '#add_module') {
         url = '/api/add_module/';
     } else if (id === '#list_pro') {
         url = '/api/project_list/1/';
