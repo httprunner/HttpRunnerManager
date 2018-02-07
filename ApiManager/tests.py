df0d5ea7e7af57e1249908f9ca720e1a8dc9c529
尹全旺 <yinquanwang@weibopay.com>
2018-02-07 15:10:39 +0800

    去除urls

--- ApiManager/tests.py
+++ ApiManager/tests.py
@@ -1,14 +1,17 @@
-from django.test import TestCase
 
 # Create your tests here.
 import os,django
 
+import requests
+from requests import request
+
+from httprunner.cli import main_ate
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HttpRunnerManager.settings")
+
 django.setup()
-from ApiManager.models import ProjectInfo, ModuleInfo
+from ApiManager.models import TestCaseInfo, ModuleInfo
 
-print(ProjectInfo.objects.all().get(pro_name__exact='上海').id)
-
-
-
-
+a = TestCaseInfo.objects.filter(belong_module=ModuleInfo.objects.get(id=25)).values_list('id')
+for i in a:
+    print(i[1])
