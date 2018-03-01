46e13731e0c8e3164ddaea47fb52bd8d6092b0a2
尹全旺 <yinquanwang@weibopay.com>
2018-03-01 17:59:20 +0800

    去掉BeautifulReport库

--- ApiManager/tests.py
+++ ApiManager/tests.py
@@ -2,16 +2,7 @@
 # Create your tests here.
 import os,django
 
-import requests
-from requests import request
-
-from httprunner.cli import main_ate
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HttpRunnerManager.settings")
 
 django.setup()
-from ApiManager.models import TestCaseInfo, ModuleInfo
-
-a = TestCaseInfo.objects.filter(belong_module=ModuleInfo.objects.get(id=25)).values_list('id')
-for i in a:
-    print(i[1])
