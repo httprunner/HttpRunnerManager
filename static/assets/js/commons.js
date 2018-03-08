5d6665773ef669cc6d3aba25a8ee35865003e827
尹全旺 <yinquanwang@weibopay.com>
2018-03-08 16:52:46 +0800

    移除本地bootstrap静态资源，common.js新增post方法

--- static/assets/js/commons.js
+++ static/assets/js/commons.js
@@ -179,3 +179,18 @@ function myAlert(data) {
     });
 }
 
+function post(URL, PARAMS) {
+    var temp = document.createElement("form");
+    temp.action = URL;
+    temp.method = "post";
+    temp.style.display = "none";
+    for (var x in PARAMS) {
+        var opt = document.createElement("input");
+        opt.name = x;
+        opt.value = PARAMS[x];
+        temp.appendChild(opt);
+    }
+    document.body.appendChild(temp);
+    temp.submit();
+    return temp;
+}
