9cb87bcb9a7ba9003d82464c59c6e348878e9de7
尹全旺 <yinquanwang@weibopay.com>
2018-04-24 10:40:18 +0800

    移除httprunner旧版本

--- ApiManager/tests.py
+++ ApiManager/tests.py
@@ -1,14 +1,11 @@
 # Create your tests here.
 import json
+import os
 
 import django
-import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HttpRunnerManager.settings")
 
 django.setup()
 
-a = {}
-print(json.loads(json.dumps(a)))
 
-print(bool('true'))
--- httprunner/__init__.py
+++
@@ -1,1 +0,0 @@
-from httprunner.task import HttpRunner
