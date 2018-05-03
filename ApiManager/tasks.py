918aac32a7726a1c2964a241ce3d55d5d712bd7d
尹全旺 <yinquanwang@weibopay.com>
2018-05-03 17:56:30 +0800

    新增周期调度运行tasks

--- ApiManager/tasks.py
+++ ApiManager/tasks.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 from ApiManager.utils.operation import add_test_reports
+from ApiManager.utils.runner import run_by_project
 from httprunner import HttpRunner, logger
 
 
@@ -17,3 +18,13 @@ def main_hrun(testset_path):
     runner.run(testset_path)
     add_test_reports(**runner.summary)
     return runner.summary
+
+
+@shared_task
+def periodic_hrun(testset_path):
+    testcases_dict = []
+    base_url = testset_path.pop('env_name')
+    if 'project' in testset_path.keys():
+        id = testset_path.pop('project')
+        testcases_dict = run_by_project(id, base_url)
+    return main_hrun(testcases_dict)
