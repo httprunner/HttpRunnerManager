703940726e463f9aff408c44792224fbbaa60ed2
尹全旺 <yinquanwang@weibopay.com>
2018-05-04 11:40:05 +0800

    修复异常bug

--- HttpRunnerManager/activator.py
+++ HttpRunnerManager/activator.py
@@ -25,12 +25,8 @@ def process(request, **kwargs):
         fun = getattr(view, fun)
 
         # 执行view.py中的函数，并获取其返回值
-
-        result = fun(request, index)
-    except TypeError:
-        result = fun(request)
+        result = fun(request, index) if index else fun(request)
     except (ImportError, AttributeError):
-        # 导入失败时，自定义404错误
         return HttpResponse(traceback.format_exc())
 
     return result
