eb74c6469f5cd28e0b6e51f970efb4e1b87664dd
尹全旺 <yinquanwang@weibopay.com>
2018-05-16 18:47:13 +0800

    排版优化

--- HttpRunnerManager/activator.py
+++ HttpRunnerManager/activator.py
@@ -1,5 +1,3 @@
-import traceback
-
 from django.shortcuts import HttpResponse
 
 
@@ -18,6 +16,7 @@ def process(request, **kwargs):
         # 执行view.py中的函数，并获取其返回值
         result = fun(request, index) if index else fun(request)
     except (ImportError, AttributeError):
-        return HttpResponse('404 Not Found')
+        raise
+        # return HttpResponse('404 Not Found')
 
     return result
