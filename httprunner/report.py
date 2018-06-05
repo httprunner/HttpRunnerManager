9696c364a1149849cbb58f22ccd0928ad26c476c
尹全旺 <yinquanwang@weibopay.com>
2018-06-05 16:25:19 +0800

    update httprunner

--- httprunner/report.py
+++ httprunner/report.py
@@ -68,7 +68,7 @@ def render_html_report(summary, html_report_name=None, html_report_template=None
         html_report_template = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
             "templates",
-            "default_report_template.html"
+            "extent_report_template.html"
         )
         logger.log_debug("No html report template specified, use default.")
     else:
@@ -78,7 +78,7 @@ def render_html_report(summary, html_report_name=None, html_report_template=None
     logger.log_debug("render data: {}".format(summary))
 
     report_dir_path = os.path.join(os.getcwd(), "reports")
-    start_datetime = summary["time"]["start_at"].strftime('%Y-%m-%d-%H-%M-%S')
+    start_datetime = summary["time"]["start_at"].replace(':', '-')
     if html_report_name:
         summary["html_report_name"] = html_report_name
         report_dir_path = os.path.join(report_dir_path, html_report_name)
@@ -126,7 +126,7 @@ def stringify_body(meta_data, request_or_response):
                     b64encode(body).decode('utf-8')
                 )
             else:
-                body = body.decode("utf-8")
+                body = escape(body.decode("utf-8"))
         except UnicodeDecodeError:
             pass
 
